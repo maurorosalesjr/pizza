1d042a1c988bd5ee56f6e83c0a0f4f6d8e73f4ea
Mauro Rosales Jr <mauro.rosales247@gmail.com>
2022-05-09 12:24:44 -0700

    wip

--- js/scripts.js
+++ js/scripts.js
@@ -15,30 +15,62 @@ Customer.prototype.orderNumber = function() {
   return orderNumber;
 }
 //////pizza builder/////
-function Pizza(toppings, size) {
+function Pizza(toppings, size, cheeze) {
   this.toppings = toppings;
   this.size = size;
+  this.cheeze = cheeze
   console.log(this.size); 
+  console.log(this.cheeze);
 }
 
+Pizza.prototype.toppingsCost = function() {
+  let cost = 0;
+  const costMap = {
+    "Pineapple" : 1,
+    "Onion" : 1,
+    "Garlic" : 1,
+    "Spinach" : 1,
+    "Bell Pepper" : 1,
+    "Shroomiez" : 1,
+    "Pepped'roni" : 1,
+    "Facon" : 1,
+    "Chicki Nuggiez" : 1,
+    "Zausage" : 1,
+    "Hyam" : 1,
+  }
+  for(let i=0; i < topppings.length; i++) {
+    let topping = topping[i];
+    let toppingCost = costMap[topping];
+    cost += toppingCost;
+  }return cost;
+}; 
 
 Pizza.prototype.crustPrice = function() {
   let crustPrice = 5;
   if(this.size === "small") {
-    crustPrice += 5;
+    crustPrice = 10;
   } else if(this.size === "medium") {
-    crustPrice += 10;
+    crustPrice = 15;
   } else if(this.size === "large") {
-    crustPrice += 17;
+    crustPrice = 22;
   }else if(this.size === "xtraLarge") {
-    crustPrice += 25;
-  } 
-  return output;
+    crustPrice = 30;
+  } else {crustPrice = 5}
+  return crustPrice;
 };
 
 
 
+Pizza.prototype.cheezeCost = function () {
+  let costOfCheeze = 1;
+  if(this.cheeze === "No Cheeze") {
+    costOfCheeze = 0;
+  } return costOfCheeze
+}
 
+function add (crustPrice, toppingsCost, costOfCheeze) {
+  return crustPrice + toppingsCost + costOfCheeze
+}
 
 
 
@@ -48,21 +80,27 @@ $(document).ready(function() {
     event.preventDefault();
     let firstName = $("#firstName").val();
     let lastName = $("#lastName").val();
-    let size = $("#size").val()
-  
-    let toppings = [];
-    toppings = $("input[type='checkbox']:checked").val();
-    //let sizePrice = parseInt(crustPrice(this.size));
+    let crustPrice = parseInt(Pizza.crustPrice);
+    console.log(crustPrice)
+    let toppingsCost = parseInt(Pizza.toppingsCost);
+    let costOfCheeze = parseInt(Pizza.cheezeCost);
     let customer = new Customer(firstName, lastName);
-    let pizza = new Pizza(toppings, size);
-    let finalPrice = parseInt(pizza.toppings + size.crustPrice);
-    
-    
+    let toppings = add(crustPrice, toppingsCost, costOfCheeze);
+    let size = $("#size").val();
+
+    let cheeze = $("input:radio[name='cheeze']").val();
+    let pizza = new Pizza(toppings, size, cheeze);
+    console.log(toppings);
+    //let topping =[]
+    //$('input[id="topping"]:checked').each(function() {
+      //topping.push(topping); 
+      //});
+    console.log(toppingsCost)
     $("#custName").text(customer.firstName + " " + customer.lastName)
     $("#waitTime").text(customer.waitTime);
     $("#orderNum").text(customer.orderNumber);
-    $("#orderRecap").text(`1 ${size} pizza with ${toppings} items`);
-    $("#finalPrice").text(finalPrice);
+    $("#orderRecap").text(`1 ${size} pizza`);
+    $("#finalPrice").text(pizza);
     $(".confrim-order").hide();
   })
 
